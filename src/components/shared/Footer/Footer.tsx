c2e2ff8ba7cbea263813eda370662840eefc4976
Sarfaraj525 <sarfaraj525@gmail.com>
2025-03-04 15:16:25 +0600

    fix the issues

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,11 +1,4 @@
-"use client";
-
-import React from "react";
-import { useTheme } from "next-themes";
-
 const Footer = () => {
-  const { theme } = useTheme();
-
   return (
     <footer className="w-full py-4 text-center text-sm transition-all div-dark">
       <p className="text-gray-500 dark:text-gray-400">
